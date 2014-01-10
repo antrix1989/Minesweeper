1a1918a58956eb0625305b4fc6b5bee06309ef83
Sergey Demchenko <antrix1989@gmail.com>
2014-01-10 20:28:02 +0200

    Initial Commit

---
+++ Minesweeper/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  Minesweeper
+//
+//  Created by Sergey Demchenko on 1/10/14.
+//  Copyright (c) 2014 antrix1989@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Minesweeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Minesweeper
+//
+//  Created by Sergey Demchenko on 1/10/14.
+//  Copyright (c) 2014 antrix1989@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ANAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ANAppDelegate class]));
+    }
+}
